f3c60f9bd38732f9fb3eafad9fba7dc3c9daa777
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-12-19 13:30:11 +0530

    Add calorie counting (day 1) solution

---
+++ 2022/Day01/Program.cs
@@ -0,0 +1,26 @@
+﻿// Number of top elves sharing their portion
+const int n = 3;
+
+// Read file containing the prompt
+const string filename = "Prompt.txt";
+string prompt = File.ReadAllText(filename);
+const string separator = "\r\n"; // Prompt values are separated by CR LF
+
+// Split contribution by each elf
+string[] contributions = prompt.Split(new string[] { separator + separator }, StringSplitOptions.TrimEntries);
+
+// Find sum of all contributions by each elf
+List<int> individualContributions = new();
+foreach (string contribution in contributions)
+{
+    string[] ledger = contribution.Split(separator, StringSplitOptions.TrimEntries);
+    int[] parsedLedger = Array.ConvertAll(ledger, x => int.Parse(x));
+    individualContributions.Add(parsedLedger.Sum());
+}
+
+// Sort individual contributions in descending order
+individualContributions = individualContributions.OrderByDescending(x => x).ToList();
+
+// Take sum of top 'n' contributions
+int topContributions = individualContributions.Take(n).Sum(x => x);
+Console.WriteLine(topContributions);
