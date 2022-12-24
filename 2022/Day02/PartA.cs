62fae8153f3d6ea2b91afbe3f5b1055a1a2b06e7
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-12-24 20:44:11 +0530

    Add Day02 prompt and solution

---
+++ 2022/Day02/PartA.cs
@@ -0,0 +1,32 @@
+﻿namespace Day02;
+
+internal class PartA
+{
+    // Tournament matrix (RPS vs RPS)
+    static readonly int[,] tournament = new int[3, 3]
+    {
+        { 3, 0, 6 },
+        { 6, 3, 0 },
+        { 0, 6, 3 },
+    };
+
+    public static void Run()
+    {
+        // Calculate result of each round in tournament and add score
+        int score = 0;
+        string[] rounds = Program.ReadPrompt();
+        foreach (string round in rounds)
+        {
+            // First response is from opponent, second response is from us (R=0, P=1, S=2)
+            int[] response = Program.ParseRoundAsInt(round);
+
+            // Score from round result
+            score += tournament[response[1], response[0]];
+
+            // Score from playing a specific action (R=1, P=2, S=3), so just adding 1
+            score += response[1] + 1;
+        }
+
+        Console.WriteLine(score);
+    }
+}
---
+++ 2022/Day02/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Day02;
+
+internal class Program
+{
+    public static void Main()
+    {
+        string part = "B";
+        if (part == "A") { PartA.Run(); }
+        if (part == "B") { PartB.Run(); }
+    }
+
+
+
+    // Read file containing the prompt -> split by newlines
+    public static string[] ReadPrompt()
+    {
+        const string filename = "Prompt.txt";
+        string prompt = File.ReadAllText(filename);
+        const string separator = "\r\n"; // Prompt values are separated by CR LF
+        return prompt.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    public static int[] ParseRoundAsInt(string round)
+    {
+        // Player responses { PlayerA [0] | Space [1] | PlayerB [2] }
+        int playerA = (int)round.ElementAt(0) - 65; // ASCII 'A' = 65
+        int playerB = (int)round.ElementAt(2) - 88; // ASCII 'X' = 88
+
+        return new int[2] { playerA, playerB };
+    }
+}
