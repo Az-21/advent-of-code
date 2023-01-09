3cfb10423b515b51b65297e7f5ad32bdbb790700
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-01-09 07:04:02 -0800

    Add part A solution

---
+++ 2015/Day01/PartA.cs
@@ -0,0 +1,26 @@
+﻿namespace Day01;
+
+internal static class PartA
+{
+  public static void Run()
+  {
+    // Read prompt
+    string prompt = Program.ReadPrompt();
+
+    // Count number of '(' and ')'
+    int floor = 0;
+
+    foreach (char character in prompt)
+    {
+
+      if (character.Equals('(')) { floor++; continue; }
+      if (character.Equals(')')) { floor--; continue; }
+
+      // Reaching here implies unexpected input
+      throw new InvalidDataException();
+    }
+
+    // Print final floor
+    Console.WriteLine(floor);
+  }
+}
---
+++ 2015/Day01/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day01;
+
+public class Program
+{
+  static void Main()
+  {
+    string part = "A";
+    if (part == "A") { PartA.Run(); }
+    //if (part == "B") { PartB.Run(); }
+  }
+
+  // Read file containing the prompt
+  public static string ReadPrompt()
+  {
+    const string filename = "Prompt.txt";
+    string prompt = File.ReadAllText(filename);
+    return prompt.Trim();
+  }
+}
