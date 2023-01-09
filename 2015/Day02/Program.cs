cfdf8687537ae9759444779717627f92785755c1
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-01-09 07:53:06 -0800

    Add program driver

---
+++ 2015/Day02/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Day02;
+
+public static class Program
+{
+  static void Main()
+  {
+    string part = "A";
+    if (part == "A") { PartA.Run(); }
+    if (part == "B") { PartB.Run(); }
+  }
+
+  // Read file containing the prompt -> split by newlines
+  public static string[] ReadPrompt()
+  {
+    const string filename = "Prompt.txt";
+    string prompt = File.ReadAllText(filename);
+    const string separator = "\r\n"; // Prompt values are separated by CR LF
+    return prompt.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+  }
+
+  // Separate dimension string on 'x' and return dimensions as int array
+  public static int[] ProcessDimensions(string dimension)
+  {
+    string[] dimensions = dimension.Split('x');
+    return Array.ConvertAll(dimensions, num => int.Parse(num));
+  }
+}
