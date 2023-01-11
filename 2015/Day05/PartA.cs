b3ef605a9cc1d89e4093eb4ba22c810cfe7b7a00
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-01-11 13:58:26 +0530

    Add day 05 part A solution

---
+++ 2015/Day05/PartA.cs
@@ -0,0 +1,34 @@
+﻿namespace Day05;
+
+internal static class PartA
+{
+  public static void Run()
+  {
+    // Read prompt
+    string[] prompt = Program.ReadPrompt();
+
+    // Tracker for "nice" strings
+    int niceStrings = 0;
+
+    // Iterate over given strings -> check if nice -> increment counter
+    foreach (string line in prompt)
+    {
+      bool isNiceString = IsNiceString(line);
+      if (isNiceString) { niceStrings++; }
+    }
+
+    // Print number of "nice" strings
+    Console.WriteLine(niceStrings);
+  }
+
+  // Perform checks for nice strings
+  private static bool IsNiceString(string input)
+  {
+    if (Program.ContainsForbiddenSubstrings(input) is true) { return false; }
+    if (Program.ContainsDoubleCharacter(input) is false) { return false; }
+    if (Program.HasAtLeastThreeVowels(input) is false) { return false; }
+
+    // Reaching here implies input string is "nice"
+    return true;
+  }
+}
---
+++ 2015/Day05/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Day05;
+
+public static class Program
+{
+  static void Main()
+  {
+    string part = "A";
+    if (part == "A") { PartA.Run(); }
+    if (part == "B") { PartB.Run(); }
+  }
+
+  // Read file containing the prompt -> split by newlines
+  public static string[] ReadPrompt()
+  {
+    const string filename = "Prompt.txt";
+    string prompt = File.ReadAllText(filename).ToLower(); // Ensure everything is in lowercase
+    const string separator = "\r\n"; // Prompt values are separated by CR LF
+    return prompt.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+  }
+
+  // Check a string against "bad" substrings
+  public static bool ContainsForbiddenSubstrings(string input)
+  {
+    // List of "bad" substrings
+    string[] forbiddenSubstrings = { "ab", "cd", "pq", "xy" };
+
+    // Using .IndexOf() instead of .Contains() for better performance | We just need boolean
+    return forbiddenSubstrings.Any(substring => input.IndexOf(substring) >= 0);
+  }
+
+  // Ensure a string contains at least three vowels
+  public static bool HasAtLeastThreeVowels(string input)
+  {
+    HashSet<char> vowels = new() { 'a', 'e', 'i', 'o', 'u' };
+    int vowelCount = input.Count(c => vowels.Contains(c));
+    return vowelCount >= 3;
+  }
+
+  // Check a string against double characters
+  public static bool ContainsDoubleCharacter(string input)
+  {
+    // List of double substrings
+    string[] doubleSubstrings =
+      Enumerable.Range('a', 26) // Start at ASCII 'a' -> End at 'a'+26 == 'z'
+                .Select(x => ((char)x).ToString() + ((char)x).ToString())
+                .ToArray();
+
+    // Using .IndexOf() instead of .Contains() for better performance | We just need boolean
+    return doubleSubstrings.Any(substring => input.IndexOf(substring) >= 0);
+  }
+}
