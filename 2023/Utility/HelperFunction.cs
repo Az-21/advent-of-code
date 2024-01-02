6142dca8cb2ada9a5e2b3aef005f9edfba072b32
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-01-02 13:00:30 +0530

    Add shared utility helper function

---
+++ 2023/Utility/HelperFunction.cs
@@ -0,0 +1,13 @@
+﻿namespace Utility;
+
+public static class Helper
+{
+  private static void Main() { }
+  public static string[] ReadPromptLines()
+  {
+    const string filename = "Prompt.txt";
+    string prompt = File.ReadAllText(filename).ToLower();
+    const string separator = "\r\n"; // Prompt values are separated by CR LF
+    return prompt.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+  }
+}
